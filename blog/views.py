4298b30119fa11db5439ebc49ab2fd2221db650a
Aaron Nichols <anichols@billiontoone.com>
2021-04-13 08:02:54 -0700

    Add post_new and post_edit views

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
 from . import models
+from . import forms
 
 
 def post_list(request):
@@ -12,3 +13,31 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(models.Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == 'POST':
+        form = forms.PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_at = timezone.now()
+            post.save()
+            return redirect('post-detail', pk=post.pk)
+    else:
+        form = forms.PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(models.Post, pk=pk)
+    if request.method == 'POST':
+        form = forms.PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_at = timezone.now()
+            post.save()
+            return redirect('post-detail', pk=post.pk)
+    else:
+        form = forms.PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
