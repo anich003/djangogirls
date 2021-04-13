ee2606d539e6b3efa7fa9c40a54cff0404ef6c3b
Aaron Nichols <anichols@billiontoone.com>
2021-04-12 20:38:34 -0700

    add views for post details

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from . import models
@@ -7,3 +7,8 @@ from . import models
 def post_list(request):
     posts = models.Post.objects.filter(published_at__lte=timezone.now()).order_by('-published_at')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(models.Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
